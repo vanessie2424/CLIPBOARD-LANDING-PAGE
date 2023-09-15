3b5d10f45de2cc15a9b82e9f548252e8a4553db8
Anazodo Vanessa <anazodovanessa19@gmail.com>
2023-09-15 11:48:47 +0100

    init

--- clipboard-landing-page/src/components/Access.jsx
+++ clipboard-landing-page/src/components/Access.jsx
@@ -1,14 +1,13 @@
 import React from "react"
-import imageDevice from "./image/image-devices.png"
 function Access (){
     return(
         <>
-         <section className="section_access">
+         <section clasnpm run buildsName="section_access">
             <h2>Access Clipboard anywhere </h2>
             <p>Whether you’re on the go, or at your computer, you can access all your 
                 Clipboard snippets in a few simple clicks. 
             </p>
-            <img src = {imageDevice} className="image_access" />
+            <img src="./image/image-devices.png" className="image_access" />
 
             <h2>Supercharge your workflow </h2>
             <p>We’ve got the tools to boost your productivity. </p>
